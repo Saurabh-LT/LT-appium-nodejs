a72ca3db9db805d581180ed08e8432389515c14a
Saurabh <saurabh@lambdatest.com>
2024-07-11 22:05:27 +0530

    Done

--- Android.js
+++ Android.js
@@ -14,14 +14,15 @@ const accessKey = process.env.LT_ACCESS_KEY || "accessKey";
  * Capabilities to be passed while running the test.
  */
 const desiredCapabilities = {
-  app: "proverbialandroid", // Enter the 'app_url' here.
+  app: "lt://proverbial-android", // Enter the 'app_url' here.
   build: "NodeJS - Android",
   name: "Sample Test NodeJS",
-  deviceName: ".*",
+  deviceName: "Galaxy S20",
   isRealMobile: true,
   platformName: "android",
+  platformVersion: "11",
   video: true,
-  visual: true
+  visual: true,
 };
 
 const driver = wd.promiseRemote(
@@ -33,15 +34,57 @@ const DEFAULT_TIMEOUT = 10000;
 /**
  * Run an android test.
  */
- async function runAndroidTest() {
+async function runAndroidTest() {
   try {
-  driver
+    driver
       .init(desiredCapabilities)
       .then(function () {
-        return driver.executeScript(`smartui.takeScreenshot=CI-Node-js-Appium`);
+        return driver.waitForElementById("color", DEFAULT_TIMEOUT);
+      })
+      .then(function (colorButton) {
+        return colorButton.click();
+      })
+      .then(function () {
+        return driver.waitForElementById("Text", DEFAULT_TIMEOUT);
+      })
+      .then(function (text) {
+        text.click();
+        return driver.waitForElementById("toast", DEFAULT_TIMEOUT);
+      })
+      .then(function (toast) {
+        toast.click();
+        return driver.waitForElementById("notification", DEFAULT_TIMEOUT);
+      })
+      .then(function (notification) {
+        notification.click();
+        return driver.waitForElementById("geoLocation", DEFAULT_TIMEOUT);
+      })
+      .then(function (geoLocation) {
+        geoLocation.click();
+        return driver.waitForElementById("buttonPage", DEFAULT_TIMEOUT);
+      })
+      .then(function (Home) {
+        Home.click();
+        return driver.waitForElementById("speedTest", DEFAULT_TIMEOUT);
+      })
+      .then(function (speedTest) {
+        speedTest.click();
+        return driver.waitForElementById("webview", DEFAULT_TIMEOUT);
+      })
+      .then(function (Browser) {
+        Browser.click();
+        return driver.waitForElementById("url", DEFAULT_TIMEOUT);
+      })
+      .then(function (url) {
+        url.type("https://www.lambdatest.com");
+        return driver.waitForElementById("find", DEFAULT_TIMEOUT);
+      })
+      .then(function (find) {
+        find.click();
+        driver.quit();
       });
   } catch (e) {
-    await driver.quit();
+    driver.quit();
   }
 }
 
